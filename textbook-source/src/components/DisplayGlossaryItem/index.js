906577190c55cd87860a915a1c5e35579fdd2ab8
Mark Ferraretto <mark@ferraretto.com>
2023-06-19 16:02:14 +0930

    Working on vital statistics

--- textbook-source/src/components/DisplayGlossaryItem/index.js
+++ textbook-source/src/components/DisplayGlossaryItem/index.js
@@ -22,6 +22,7 @@ export function DisplayGlossaryItem(props) {
             text: 'block',
             plural: 'blocks' }
         ],
+        ['boolean', { text: 'boolean'} ],
         ['brace', {
             text: 'brace',
             plural: 'braces' }
