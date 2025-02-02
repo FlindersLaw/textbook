c557bf490059a62bc402592a3dc995db7d832252
Mark Ferraretto <mark@ferraretto.com>
2025-02-02 18:07:21 +1030

    Temporarily moved previous coding exercises out the way and created new exercises for 2025 iteration. Coding 1 Exercise 1 complete.

---
+++ textbook-source/docs/coding_exercises/_resources/index.js
@@ -0,0 +1,3 @@
+export {default as ChapterVideoTable} from './chapter_video_table.mdx';
+export {default as Section58CalculatorChart} from './s58_calculator_chart.mdx';
+export {default as Section58CalculatorScreens} from './s58_calculator_screens.mdx';
