d332a4fd63fd0a8960fac94b4ee3e1ff8f6e7c19
Mark Ferraretto <mark@ferraretto.com>
2025-02-03 22:20:42 +1030

    Added Coding 3 Exercise. Tidied up folder structure.

--- textbook-source/docs/coding_exercises/_resources/index.js
+++ textbook-source/docs/coding_exercises/_resources/index.js
@@ -1,3 +1,5 @@
 export {default as ChapterVideoTable} from './chapter_video_table.mdx';
 export {default as Section58CalculatorChart} from './s58_calculator_chart.mdx';
 export {default as Section58CalculatorScreens} from './s58_calculator_screens.mdx';
+export {default as Section52CalculatorChart} from './s52_calculator_chart.mdx';
+export {default as Section52CalculatorScreens} from './s52_calculator_screens.mdx';
